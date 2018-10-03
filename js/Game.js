0479456aace74b83a5b2ec527df84880ed6cf675
Ovi# <covidiu97@gmail.com>
2018-10-03 12:08:13 +0200

    up 1

--- js/Game.js
+++ js/Game.js
@@ -1,1 +1,17 @@
-class Game {}
+class Game {
+    constructor(ready) {
+        this.board = new Board;
+        this.players = [];
+        this.ready = false;
+    }
+    /** 
+     * Creates two player objects
+     * @return  {Array}    An array of two Player objects.
+     */
+
+    createPlayers() {
+        let players = [ new Player('Ovi', '#e15258', 1, true),
+                        new Player('AI', '#e59a13', 2)]
+        return players;
+    }
+}
--- js/Space.js
+++ js/Space.js
@@ -1,1 +1,7 @@
-class Space {}
+class Space {
+    constructor(x, y) {
+        [this.x, this.y] = [x, y];
+        this.id = `space-${x}-${y}`;
+        this.token = null;
+    }
+}
